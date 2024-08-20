ece037b706658ac1bc790fd09686abbea9ed47e2
CBackyx <cbackyx@gmail.com>
2024-08-20 16:00:24 +0800

    Add eval.

---
+++ eval/eval_func.cpp
@@ -0,0 +1,19 @@
+#include <cryptoTools/Common/CLP.h>
+
+#include "aby3_tests/aby3_tests.h"
+#include <tests_cryptoTools/UnitTests.h>
+#include <aby3-ML/main-linear.h>
+#include <aby3-ML/main-logistic.h>
+#include "tests_cryptoTools/UnitTests.h"
+#include "cryptoTools/Crypto/PRNG.h"
+
+#include "aby3-Graph_tests/tests.h"
+
+using namespace oc;
+using namespace aby3;
+
+
+void eval_ours()
+{
+	Sh3_Graph_CC_test();
+}
---
+++ eval/main.cpp
@@ -0,0 +1,9 @@
+
+#include "eval_func.h"
+
+
+int main(int argc, char** argv)
+{
+	eval_ours();
+	return 0;
+}
