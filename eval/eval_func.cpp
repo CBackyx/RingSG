c4396f30d01600eb9da66705028128b8d1d9c1d6
CBackyx <cbackyx@gmail.com>
2024-08-20 16:22:21 +0800

    Add eval.

--- eval/eval_func.cpp
+++ eval/eval_func.cpp
@@ -1,3 +1,5 @@
+#include "eval_func.h"
+
 #include <cryptoTools/Common/CLP.h>
 
 #include "aby3_tests/aby3_tests.h"
@@ -9,11 +11,24 @@
 
 #include "aby3-Graph_tests/tests.h"
 
+std::mutex print_duration_mutex;
+
+void print_duration(std::chrono::_V2::system_clock::time_point t1, std::string tag) {
+    print_duration_mutex.lock();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::cout << "::" << tag << " took "
+              << ((double)std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count()) / 1000
+              << " seconds\n";
+    print_duration_mutex.unlock();
+}
+
 using namespace oc;
 using namespace aby3;
 
 
 void eval_ours()
 {
+    auto t_tmp = std::chrono::high_resolution_clock::now();
 	Sh3_Graph_CC_test();
+    print_duration(t_tmp, "ours");
 }
--- eval/eval_func.h
+++ eval/eval_func.h
@@ -1,3 +1,9 @@
 #pragma once
 
+#include <chrono>
+#include <mutex>
+#include <string>
+#include <iostream>
+
 void eval_ours();
+void print_duration(std::chrono::_V2::system_clock::time_point t1, std::string tag);
